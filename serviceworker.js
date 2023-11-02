e36861d408504ee26d173e728be3205ef433d633
MANDAR RAI <mandarrai@gmail.com>
2023-11-02 10:49:04 +0530

    Upload to repo

--- serviceworker.js
+++
@@ -1,9 +0,0 @@
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (tab.url?.startsWith("chrome://")) { return undefined; }
-    if (changeInfo.status == 'complete') {
-        chrome.scripting.executeScript({
-            files: ['script.js'],
-            target: { tabId: tab.id }
-        })
-    }
-}); 
